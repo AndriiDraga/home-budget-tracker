911c610b9dca544ae73d7cae5359e39ab717fe14
Andrew <andrijshepit@gmail.com>
2026-08-17 21:55:45 +0300

    feat: add draft index view to verify models and templates

--- finance/views.py
+++ finance/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+
+from django.shortcuts import render
+from .models import Category, Account, Transaction
+
+
+def index(request):
+    context = {
+        "category_count": Category.objects.count(),
+        "account_count": Account.objects.count(),
+        "transaction_count": Transaction.objects.count(),
+    }
+    return render(request, "finance/index.html", context)
