b858ee42928b3543985a0501342efab5d13ce154
Andrew <andrijshepit@gmail.com>
2026-08-24 23:28:40 +0300

    chore: install whitenoise and add STATIC_ROOT for collectstatic

--- budget_tracker/settings/base.py
+++ budget_tracker/settings/base.py
@@ -102,6 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
